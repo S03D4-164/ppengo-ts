91141861db45a11d35fe4a7b25d617f38642650e
S03D4-164 <12199393+S03D4-164@users.noreply.github.com>
2025-10-26 23:13:03 +0900

    feat🚀: Configure Nginx and Vite for /ppengo-ts deployment and proxy improvements

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -2,11 +2,12 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-  base: "/ppengo/",
+  base: "/ppengo-ts/",
   plugins: [vue()],
   server: {
     host: true,
     port: 5173,
+    //open: "/ppengo-ts/",
     proxy: {
       "/ppengo/api": {
         target: "http://127.0.0.1:3000",
@@ -15,17 +16,17 @@ export default defineConfig({
     },
   },
   build: {
-    outDir: "../nginx/prod/ppengo",
+    outDir: "../nginx/prod/ppengo-ts",
     emptyOutDir: true,
     minify: false,
     cssMinify: false,
-    sourcemap: true,
+    //sourcemap: true,
     rollupOptions: {
       output: {
         entryFileNames: "assets/js/[name]-[hash].js",
         chunkFileNames: "assets/js/[name]-[hash].js",
-        inlineDynamicImports: true,
-        manualChunks: undefined,
+        //inlineDynamicImports: true,
+        //manualChunks: undefined,
         assetFileNames: (assetInfo) => {
           const name = assetInfo.name || "";
           if (name.endsWith(".css")) {
